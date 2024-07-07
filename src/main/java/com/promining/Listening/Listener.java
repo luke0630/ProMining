0f48d96ab4ef7b85f7d49300705bf8a66d0f72d4
luke0630 <ryuto-0915@outlook.jp>
2024-07-07 13:24:37 +0900

    first commit

---
+++ src/main/java/com/promining/Listening/Listener.java
@@ -0,0 +1,8 @@
+package com.promining.Listening;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class Listener implements org.bukkit.event.Listener {
+    public void onBreakBlock(BlockBreakEvent event) {
+
+    }
+}
