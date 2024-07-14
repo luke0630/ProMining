3130c0e00cd8f31d81c252bffc94094c502563fd
luke0630 <ryuto-0915@outlook.jp>
2024-07-14 11:43:06 +0900

    Revert "add: VIP系のメソッド追加"
    This reverts commit dceb61982e6d5923f41299b0a46c555a01dc0b65.

--- src/main/java/com/promining/Function/VIPFunction.java
+++ src/main/java/com/promining/Function/VIPFunction.java
@@ -1,15 +1,11 @@
 package com.promining.Function;
 
 import com.promining.Data.VIPData;
-import org.bukkit.block.Block;
-import org.bukkit.block.data.BlockData;
 
 import static com.promining.Data.Data.*;
 
 public class VIPFunction {
     public static void RemoveVIP(VIPData data) {
         vipData.remove(data);
-    }
-    public static void AddVIPMiningBlock(Block block) {
     }
 }
