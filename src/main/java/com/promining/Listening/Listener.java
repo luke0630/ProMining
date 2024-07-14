e899a4b66b4514c27d450e632e13f9221e1369f4
luke0630 <ryuto-0915@outlook.jp>
2024-07-14 11:43:10 +0900

    Revert "first commit"
    This reverts commit 0f48d96ab4ef7b85f7d49300705bf8a66d0f72d4.

--- src/main/java/com/promining/Listening/Listener.java
+++
@@ -1,8 +0,0 @@
-package com.promining.Listening;
-import org.bukkit.event.block.BlockBreakEvent;
-
-public class Listener implements org.bukkit.event.Listener {
-    public void onBreakBlock(BlockBreakEvent event) {
-
-    }
-}
